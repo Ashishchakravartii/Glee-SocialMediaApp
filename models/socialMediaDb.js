c337f2711ebb75c0bea0c1cd3d8336a362f219c7
Ashish Chakravarti <chakravartiashish2406@gmail.com>
2023-11-07 11:17:14 +0530

    join btn width increase

--- models/socialMediaDb.js
+++ models/socialMediaDb.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_CONNECT_URI)
+  // .connect(process.env.MONGODB_CONNECT_URI)
+  .connect("mongodb://127.0.0.1:27017/socialMedia")
   .then(() => {
     console.log("Db connected");
   })
